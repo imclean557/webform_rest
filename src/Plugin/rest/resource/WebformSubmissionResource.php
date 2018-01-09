53313728f66d291391bc6194fb84459e17c0181d
Ian McLean <imclean@digitalink.com.au>
2018-01-09 16:26:57 +1100

    Issue #2908727 by imclean: Retrieve webform data via REST

--- src/Plugin/rest/resource/WebformSubmissionResource.php
+++ src/Plugin/rest/resource/WebformSubmissionResource.php
@@ -2,13 +2,13 @@
 
 namespace Drupal\webform_rest\Plugin\rest\resource;
 
-use Drupal\webform\Entity\Webform;
+use Drupal\webform\Entity\WebformSubmission;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ModifiedResourceResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
- * Creates a resource for retrieving webform elements.
+ * Creates a resource for retrieving webform submission data.
  *
  * @RestResource(
  *   id = "webform_rest_submission",
@@ -24,12 +24,42 @@ class WebformSubmissionResource extends ResourceBase {
    *  Retrieve submission data.
    *
    * @param string $webform_id
-   *   The ID of the webform.
+   *   Webform ID.
    *
-   * @param integer $sid
-   *   Webform submission ID.
+   * @param int $sid
+   *   Submission ID.
+   *
+   * @return \Drupal\rest\ModifiedResourceResponse
+   *   HTTP response object containing webform submission.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws HttpException in case of error.
    */
   public function get($webform_id, $sid) {
-    
+
+    if (empty($webform_id) || empty($sid)) {
+      $errors = [
+        'error' => [
+          'message' => 'Both webform ID and submission ID are required.'
+        ]
+      ];
+      return new ModifiedResourceResponse($errors);
+    }
+
+    // Load the webform submission.
+    $webform_submission = WebformSubmission::load($sid);
+
+    // Basic check to see if something's returned.
+    if ($webform_submission) {
+
+      // Grab submission data.
+      $data = $webform_submission->getData();
+
+      // Return the submission.
+      return new ModifiedResourceResponse($data);
+    }
+
+    throw new HttpException(t("Can't load webform submission."));
   }
+
 }
